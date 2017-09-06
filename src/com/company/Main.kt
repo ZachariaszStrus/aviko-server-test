56351007923861ee3d291465714aec2fc34e9c05
dawid <dawzwoli>
2017-09-06 18:22:11 +0200

    purchases dawid2

--- src/com/company/Main.kt
+++ src/com/company/Main.kt
@@ -4,7 +4,7 @@ package com.company
 fun main(args: Array<String>) {
     //SignUpWorker.start(6000)
 
-    PurchaseWorker.start("users_zaki_1.json", "purchases_zaki_1.json")
+    PurchaseWorker.start("users_dawid_2.json", "purchases_dawid_2.json")
 //    PurchaseWorker.start("users2.json", "purchases2.json")
 }
 
