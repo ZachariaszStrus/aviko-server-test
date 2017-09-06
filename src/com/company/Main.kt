45293cfaad7b43ac5c8040525245cade9a1051ed
dawid <dawzwoli>
2017-09-06 10:29:08 +0200

    iamgegenerator ready

--- src/com/company/Main.kt
+++ src/com/company/Main.kt
@@ -1,10 +1,13 @@
 package com.company
 
+import com.company.generator.ImageGenerator
 
 fun main(args: Array<String>) {
     //SignUpWorker.start(6000)
 
-    PurchaseWorker.start("users2.json", "purchases.json")
+    //PurchaseWorker.start("users2.json", "purchases.json")
+
+    ImageGenerator.init()
 }
 
 
--- src/com/company/generator/ImageGenerator.kt
+++ src/com/company/generator/ImageGenerator.kt
@@ -4,16 +4,34 @@ import com.company.model.Photo
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import com.google.gson.stream.JsonReader
-import com.google.gson.stream.JsonWriter
 import java.io.FileReader
-import java.io.FileWriter
+import java.util.*
 
 object ImageGenerator {
     private val gson = Gson()
+    private val random = Random()
+    private val photos = this.getList("photos.json")
 
-    fun init() {
-        val photos = this.getList("photos.json")
-    }
+    private val purchProbArray = arrayOf(
+            PurchProbEntry(1, 4),
+            PurchProbEntry(2, 8),
+            PurchProbEntry(3, 12),
+            PurchProbEntry(4, 16),
+            PurchProbEntry(5, 20),
+            PurchProbEntry(6, 20),
+            PurchProbEntry(7, 10),
+            PurchProbEntry(8, 5),
+            PurchProbEntry(9, 3),
+            PurchProbEntry(10, 2)
+    )
+
+    private val photoProbArray = arrayOf(
+            PurchProbEntry(2, 70),
+            PurchProbEntry(3, 20),
+            PurchProbEntry(4, 5),
+            PurchProbEntry(5, 3),
+            PurchProbEntry(6, 2)
+    )
 
     private fun getList(filename: String): List<Photo> {
         val reader = JsonReader(FileReader(filename))
@@ -23,15 +41,50 @@ object ImageGenerator {
 
     fun getPhotosList(): ArrayList<Photo> {
         val result = ArrayList<Photo>()
-        result.add(Photo("data:image/jpeg;base64, null"))
-        result.add(Photo("data:image/jpeg;base64, null1"))
-        result.add(Photo("data:image/jpeg;base64, null2"))
+
+        val count = this.randomPhotoNum()
+        for(i in 1..count)
+            result.add(this.randomPhoto())
 
         return result
     }
 
+    private fun randomPhoto(): Photo {
+        return this.photos[this.random.nextInt(this.photos.size)]
+    }
+
     fun getPurchasesNumber(): Int {
-        return 2
+        val len = this.purchProbArray.size
+        var max = 0
+        this.purchProbArray.forEach { entry ->
+            if(entry.prob > max)
+                max = entry.prob
+        }
+
+        while(true) {
+            val index: Int = this.random.nextInt(len)
+            val value: Int = this.random.nextInt(max)
+
+            if(value < this.purchProbArray[index].prob)
+                return this.purchProbArray[index].count
+        }
+    }
+
+    private fun randomPhotoNum(): Int {
+        val len = this.photoProbArray.size
+        var max = 0
+        this.photoProbArray.forEach { entry ->
+            if(entry.prob > max)
+                max = entry.prob
+        }
+
+        while(true) {
+            val index: Int = this.random.nextInt(len)
+            val value: Int = this.random.nextInt(max)
+
+            if(value < this.photoProbArray[index].prob)
+                return this.photoProbArray[index].count
+        }
     }
 
     fun getProductsInPurchase(): Int {
@@ -41,4 +94,6 @@ object ImageGenerator {
     fun getPurchaseStatus(): Int {
         return 1 // 1/0
     }
+
+    class PurchProbEntry(val count: Int, val prob: Int)
 }
