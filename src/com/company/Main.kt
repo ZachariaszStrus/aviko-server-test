10426b16acf49e942f9fd641f93cfaf688fc362a
zacstrus <zachariasz.strus@gmail.com>
2017-09-06 10:30:35 +0200

    Merge branch 'master' of https://github.com/ZachariaszStrus/aviko-server-test
    # Conflicts:
    #	src/com/company/generator/ImageGenerator.kt

--- src/com/company/Main.kt
+++ src/com/company/Main.kt
@@ -1,10 +1,15 @@
 package com.company
 
+import com.company.service.AccountService
+
 
 fun main(args: Array<String>) {
     //SignUpWorker.start(6000)
 
-    PurchaseWorker.start("users2.json", "purchases.json")
+//    PurchaseWorker.start("users2.json", "purchases.json")
+
+    val s = AccountService.authorizeAccounts("2017-09-05 12:00:00")
+    System.out.println(s)
 }
 
 
